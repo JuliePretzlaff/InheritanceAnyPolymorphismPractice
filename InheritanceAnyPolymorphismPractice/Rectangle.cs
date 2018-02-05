7a3bfe7d029a29ca7eab07ac74cf53f9ecf22fe6
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-02-05 11:56:42 -0500

    first derived class added

---
+++ InheritanceAnyPolymorphismPractice/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAnyPolymorphismPractice
+{
+    public class Rectangle : Shape
+    {
+        //fields:
+        private double area;
+
+        //property:
+        public double Area
+        {
+            get {  return this.area; }
+            set { this.area = value; }
+        }
+
+        //constructor:
+        public Rectangle(double area)
+        {
+            this.area = area;
+        }
+    }
+}
---
+++ InheritanceAnyPolymorphismPractice/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAnyPolymorphismPractice
+{
+    class Shape
+    {
+        //fields:
+        private int sides;
+
+
+        //properties:
+        public int Sides
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        //constructors:
+        public Shape()
+        {
+            //default constructor
+        }
+       
+        public Shape(int sides)
+        {
+            this.sides = Sides;
+        }
+
+        //methods
+
+    }
+}
