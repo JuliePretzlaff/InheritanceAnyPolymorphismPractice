e52a6ce4e0820817fa1d2555ff1975e9c28e1ddd
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-02-05 13:47:29 -0500

    added animal class and two derived classes

---
+++ InheritanceAnyPolymorphismPractice/RattleSnake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAnyPolymorphismPractice
+{
+    class RattleSnake : Animal
+    {   //field:
+        int snakeLength;
+
+        //property:
+        public int SnakeLength
+        {
+            get { return this.snakeLength; }
+            set { this.snakeLength = value; }
+        }
+
+
+       //constructor:
+        public RattleSnake(int length) : base("reptile", false)
+        {
+            this.snakeLength = length;
+            this.NumberOfLegs = 0;
+        }
+
+
+        //methods:
+        public void Rattle()
+        {
+            Console.WriteLine("\a");
+        }
+    }
+}
--- InheritanceAnyPolymorphismPractice/Rectangle.cs
+++ InheritanceAnyPolymorphismPractice/Rectangle.cs
@@ -19,7 +19,9 @@ namespace InheritanceAnyPolymorphismPractice
         }
 
         //constructor:
-        public Rectangle(double area)
+        //derived class can't inherit constructors from base class.  however, constructors of base
+        //class can be accessed using "base" keyword.
+        public Rectangle(double area) : base(4) //sides be set to four, since that's the parameter for the base class (can choose which constructor you're referencing)
         {
             this.area = area;
         }
